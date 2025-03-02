03849cdd3f8fc9550ee512d758f48c8a3d5e049e
anais <anaismotquin@gmail.com>
2025-03-02 23:02:17 +0100

    eslint

--- src/components/profile/ReportedCats.js
+++ src/components/profile/ReportedCats.js
@@ -35,7 +35,7 @@ const ReportedCats = ({ reportedCats, onDelete, onEdit, successMessage }) => {
       setLoadingMatches(loading);
     };
     fetchMatchCounts();
-  }, [reportedCats]);
+  }, [reportedCats, findPotentialMatches]);
 
   const handleDelete = (catStatusId) => {
     if (window.confirm("Êtes-vous sûr de vouloir supprimer ce chat signalé ?")) {
