5ac1bef80799cc35ebb6d3927c14d2938844802b
anais <anaismotquin@gmail.com>
2025-04-19 22:38:23 +0200

    Configure i18next scanner and add initial translation files for EN, FR, NL languages

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -9,8 +9,28 @@ module.exports = {
   ],
   output: './public/locales/{{lng}}/{{ns}}.json',
   options: {
+    // Ajoute 't' pour détecter les hooks useTranslation()
+    func: {
+      list: ['t', 'i18next.t', 'i18n.t'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    // Pour détecter les balises <Trans> si tu en utilises
+    trans: {
+      component: 'Trans',
+      i18nKey: 'i18nKey',
+      defaultsKey: 'defaults',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      fallbackKey: false,
+      supportBasicHtmlNodes: true,
+      keepBasicHtmlNodesFor: ['br', 'strong', 'i', 'p'],
+      acorn: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
     debug: true,
-    removeUnusedKeys: true,
+    removeUnusedKeys: true, // Supprime les clés non utilisées automatiquement
+    // Conseil : ajoute des valeurs par défaut dans tes appels t('key', 'valeur') pour pré-remplir les fichiers de traduction
     sort: true,
     lngs: ['fr', 'en', 'nl'],
     defaultLng: 'fr',
@@ -20,7 +40,7 @@ module.exports = {
       savePath: './public/locales/{{lng}}/{{ns}}.json',
     },
     ns: ['translation'],
-    defaultValue: '',
+    defaultValue: '', // Laisse vide, mais privilégie les valeurs par défaut dans le code
     keySeparator: false, // Permet d'utiliser des clés avec des points
     nsSeparator: false,  // Permet d'utiliser des namespaces avec des points
     interpolation: {
